665beeb0546191fb595505afaea9fcdab5dab440
Linet <teweilinet@gmail.com>
2024-05-24 23:27:50 +0300

    instance methods

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,14 @@
 
 class Dog:
     # Class body goes here
+ def __init__(self):
+   pass
+ def bark(self):
+    print("Woof!")
+ def sit(self):
+    print("The dog is sitting.")
+fido=Dog()
+fido.bark()
+fido.sit()
 
-    #Instance method definition
-    pass
+pass
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,15 @@
 
 class Person:
     # Class body goes here
-
-    #Instance method definition
+  def __init__(self):
     pass
+  def talk(self):
+     print("Hello World!")
+  def walk(self):
+    print("The person is walking.")
+    #Instance method definition
+person1=Person()
+
+person1.talk()
+person1.walk()
+pass
